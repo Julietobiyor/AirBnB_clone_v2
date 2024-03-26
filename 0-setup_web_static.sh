5ce08adfe8ff7e21535d68904d2450d7424d8d29
Julietobiyor <obiyorjuliet@gmail.com>
2024-03-26 05:44:59 -0700

    deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,68 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
-
+# Bash script that sets up your web servers for the deployment of web_static
 apt-get update
 apt-get install -y nginx
+mkdir -p /data/web_static/releases/test/ /data/web_static/shared/ /var/www/html /var/www/error
+chmod -R 755 /var/www
+echo 'Hello World!' > /var/www/html/index.html
+echo -e "Ceci n\x27est pas une page" > /var/www/error/404.html
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
+# Create a symbolic link
+[ -d /data/web_static/current ] && rm -rf /data/web_static/current
 ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+# Server configuration
+CONFIG="server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	server_name _;
+	index index.html index.htm;
+	error_page 404 /404.html;
+	add_header X-Served-By \$hostname;
+	location / {
+		root /var/www/html/;
+		try_files \$uri \$uri/ =404;
+	}
+	location /hbnb_static/ {
+		alias /data/web_static/current/;
+		try_files \$uri \$uri/ =404;
+	}
+	if (\$request_filename ~ redirect_me) {
+		rewrite ^ https://st-pardon.github.io/portfolio-landing-page permanent;
+	}
+	location = /404.html {
+		root /var/www/error/;
+		internal;
+	}
+}"
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $hostname;
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 http://github.com/Julietobiyor;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
+# HTML file
+HTML_FILE="<!DOCTYPE html>
+<html lang='en-US'>
+	<head>
+		<meta charset=\"utf-8\">
+		<meta name=\"description\" content=\"AirBnB Clone\">
+		<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+		<title>AirBnB Clone</title>
+	</head>
+	<body>
+		<header>
+			<h1>AirBnB</h1>
+		</header>
+		<h2>Welcome to AirBnB!</h2>
+	<body>
+</html>
+"
 
-service nginx restart
+echo -e "$HTML_FILE" > /data/web_static/releases/test/index.html
+bash -c "echo -e '$CONFIG' > /etc/nginx/sites-available/default"
+ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+chown -R ubuntu:ubuntu /data/
+
+# start or restart server
+if [ "$(pgrep -c nginx)" -le 0 ]; then
+	service nginx start
+else
+	service nginx restart
+fi
